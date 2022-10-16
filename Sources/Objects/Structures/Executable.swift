f80450df52fb7ce5f541eaa9b85a56bcf75b2a51
Václav Brož <vaclav.broz@icloud.com>
2022-10-16 10:15:08 +0200

    Use bootstrap Environment method in Executable main

--- Sources/Objects/Structures/Executable.swift
+++ Sources/Objects/Structures/Executable.swift
@@ -5,11 +5,11 @@ import Vapor
 
 @main struct Executable {
     static func main() throws {
-        var env = try Environment.detect()
-        try LoggingSystem.bootstrap(from: &env)
-        let app = Application(env)
-        defer { app.shutdown() }
-        try app.configure()
-        try app.run()
+        var environment = try Environment.bootstrap()
+        try LoggingSystem.bootstrap(from: &environment)
+        let application = Application(environment)
+        defer { application.shutdown() }
+        try application.configure()
+        try application.run()
     }
 }
