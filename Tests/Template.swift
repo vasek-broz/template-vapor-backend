10b0fb719f19b42bec85438a098e32fc02ea89c8
Václav Brož <vaclav.broz@icloud.com>
2022-10-16 06:56:09 +0200

    Add database_step to GitHub workflow and uncomment tests

--- Tests/Template.swift
+++ Tests/Template.swift
@@ -5,20 +5,20 @@
 import XCTVapor
 
 final class TemplateTests: XCTestCase {
-//    func testGettingAllTemplates() async throws {
-//        let application = Application(.testing)
-//        defer { application.shutdown() }
-//        try application.configure()
-//        
-//        try await TemplateModel(templateField: "first").save(on: application.database)
-//        try await TemplateModel(templateField: "second").save(on: application.database)
-//        
-//        try application.test(.GET, "template/all", afterResponse: { response in
-//            XCTAssertEqual(response.status, .ok)
-//            let decodedResponseContent = try response.content.decode([TemplateModel].self)
-//            XCTAssertEqual(decodedResponseContent.count, 2)
-//            XCTAssertEqual(decodedResponseContent[0].templateField, "first")
-//            XCTAssertEqual(decodedResponseContent[1].templateField, "second")
-//        })
-//    }
+    func testGettingAllTemplates() async throws {
+        let application = Application(.testing)
+        defer { application.shutdown() }
+        try application.configure()
+        
+        try await TemplateModel(templateField: "first").save(on: application.database)
+        try await TemplateModel(templateField: "second").save(on: application.database)
+        
+        try application.test(.GET, "template/all", afterResponse: { response in
+            XCTAssertEqual(response.status, .ok)
+            let decodedResponseContent = try response.content.decode([TemplateModel].self)
+            XCTAssertEqual(decodedResponseContent.count, 2)
+            XCTAssertEqual(decodedResponseContent[0].templateField, "first")
+            XCTAssertEqual(decodedResponseContent[1].templateField, "second")
+        })
+    }
 }
