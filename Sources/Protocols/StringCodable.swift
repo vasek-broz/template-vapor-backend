3df1614fc22e463d22add99535a4608ccc21961f
Václav Brož <vaclav.broz@icloud.com>
2023-01-31 14:12:50 +0100

    Remove RedisHashCoding

--- Sources/Protocols/StringCodable.swift
+++
@@ -1,7 +0,0 @@
-//  StringCodable.swift
-//  Created by Václav Brož on 30/1/2023.
-
-protocol StringCodable: CustomStringConvertible, Codable {
-    init?(from string: String)
-    var string: String { get }
-}
